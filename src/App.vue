6acdc9ba60fd2a74b2b35a6e664174920948f038
artvezhl <artvezhl@gmail.com>
2022-06-22 15:32:36 +0300

    add map with points

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,31 @@
   <v-app>
     <NavMenu />
     <v-main>
-      Main grfkerng njretngrt njkrtknjrgjkrnjrg
+      <MapBox :data="geoData" />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import "mapbox-gl/dist/mapbox-gl.css";
 import NavMenu from "@/components/NavMenu";
+import MapBox from "@/components/MapBox";
+import geoJsonData from "@/mock/index.js";
 
 export default {
   name: 'App',
   components: {
+    MapBox,
     NavMenu,
   },
+  data: () => ({
+    geoData: geoJsonData,
+  }),
 };
 </script>
+
+<style scoped>
+#map {
+  height: 100%;
+}
+</style>
